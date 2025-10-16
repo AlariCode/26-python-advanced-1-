4b03fd3a1d73ae5ccab5466db3e1dec5b450e138
Alaricode <antonlarichev@gmail.com>
2025-10-16 11:35:06 +0300

    Статичный метод

--- main.py
+++ main.py
@@ -1,55 +1,19 @@
 """Демо модуль для курса"""
-
-# Герой, у которого есть имя, hp, inventory list и пометка жив ли он
-# Нужно сделать методы take_damage, heal, add_item, show_status
+from datetime import date
 
 
-class Hero:
-    """Герой игры"""
+class Book:
+    """Книга"""
 
-    def __init__(self, name: str):
-        self.name = name
-        self.hp = 100
-        self.inventory: list[str] = []
-        self.is_alive = True
+    def __init__(self, title: str, year: int):
+        self.title = title
+        self.year = year
 
-    def take_damage(self, amount: int):
-        """Получение урона"""
-        if not self.is_alive:
-            print(f"{self.name} уже повержен")
-            return
-        self.hp -= amount
-        if self.hp <= 0:
-            self.hp = 0
-            self.is_alive = False
-            print(f"{self.name} теперь повержен")
-        else:
-            print(f"{self.name} получил {amount} урона")
-
-    def heal(self, amount):
-        """Лечение"""
-        if not self.is_alive:
-            print(f"{self.name} уже повержен")
-            return
-        self.hp = min(self.hp + amount, 100)
-        print(f"{self.name} восстановил {amount} HP. Текущие HP {self.hp}")
-
-    def add_item(self, item: str):
-        """Добавление предмета"""
-        self.inventory.append(item)
-        print(f"{self.name} получил предмет {item}")
-
-    def show_status(self):
-        """Показ статуса"""
-        status = "Жив" if self.is_alive else "Повержен"
-        print(
-            f"{self.name} - HP: {self.hp} - Инвентарь: {self.inventory} [{status}]")
+    @staticmethod
+    def years_since(year: int) -> int:
+        """Метод получения возвраста"""
+        return date.today().year - year
 
 
-hero = Hero("Вася")
-hero.add_item("Меч")
-hero.show_status()
-hero.take_damage(30)
-hero.take_damage(60)
-hero.heal(50)
-hero.show_status()
+book = Book("Властелин колец", 1985)
+print(Book.years_since(book.year))
