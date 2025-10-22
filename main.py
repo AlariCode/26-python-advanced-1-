ef250d37e2c5137748ff2564f9895b18583d0c0c
Alaricode <antonlarichev@gmail.com>
2025-10-22 10:52:02 +0300

    Упражнение - декоратор retry

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 """Демо модуль для курса"""
 
+import random
 
-def repeat(times: int):
+
+def retry(times: int):
     def decorator(func):
         def wrapper(*args, **kwargs):
-            for _ in range(times):
-                func(*args, **kwargs)
+            for attemp in range(1, times + 1):
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    print(f"Попытка {attemp} не удалась: {e}")
+                    if attemp == times:
+                        print("Все попытки завершены")
         return wrapper
     return decorator
 
 
-@repeat(3)
-def hello():
-    print("Привет!")
+@retry(3)
+def unstable():
+    """Иногда падает с ошибкой"""
+    if random.random() < 0.7:
+        raise ValueError("Ошибка соединения")
+    print("✅ Успешное выполнение")
 
 
-hello()
+unstable()
