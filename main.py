c5e93b3251186680ba38ecc63667ea43d60957b5
Alaricode <antonlarichev@gmail.com>
2025-10-22 17:24:46 +0300

    Классы декораторы

--- main.py
+++ main.py
@@ -3,25 +3,38 @@
 from functools import wraps
 
 
-def limit_calls(max_calls: int):
-    def decorator(fn):
+class Limit:
+    def __init__(self, count: int):
+        self.count = count
+
+    def __call__(self, fn):
         @wraps(fn)
-        def wrapper(self, *args, **kwargs):
-            count_arrt = f"_{fn.__name__}_count"
-            current = getattr(self, count_arrt, 0)
-            if current >= max_calls:
+        def wrapper(*args, **kwargs):
+            if self.count <= 0:
                 raise RuntimeError("Call limit exceeded")
-            setattr(self, count_arrt, current + 1)
-            print(f"[LOG] {fn.__qualname__} called {current + 1}/{max_calls}")
-            return fn(self, *args, **kwargs)
+            self.count -= 1
+            return fn(*args, **kwargs)
         return wrapper
-    return decorator
+
+# def limit_calls(max_calls: int):
+#     def decorator(fn):
+#         @wraps(fn)
+#         def wrapper(self, *args, **kwargs):
+#             count_arrt = f"_{fn.__name__}_count"
+#             current = getattr(self, count_arrt, 0)
+#             if current >= max_calls:
+#                 raise RuntimeError("Call limit exceeded")
+#             setattr(self, count_arrt, current + 1)
+#             print(f"[LOG] {fn.__qualname__} called {current + 1}/{max_calls}")
+#             return fn(self, *args, **kwargs)
+#         return wrapper
+#     return decorator
 
 
 class Engine:
     """Двигатель"""
 
-    @limit_calls(3)
+    @Limit(3)
     def start(self):
         """Запуск"""
         print("🚗 Двигатель запущен!")
