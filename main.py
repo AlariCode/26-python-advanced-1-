e56967a25fad282f860714f4f9b0c7328e9c6a9c
Alaricode <antonlarichev@gmail.com>
2025-10-22 16:04:35 +0300

    Дескриптор

--- main.py
+++ main.py
@@ -1,31 +1,26 @@
 """Демо модуль для курса"""
-from functools import wraps
 
 
-def log_call(fn):
-    """Логирует вызов"""
-    @wraps(fn)
-    def wrapper(*args, **kwargs):
-        print(f"[LOG] {fn.__qualname__} args={args} kwargs={kwargs}")
-        return fn(*args, **kwargs)
-    return wrapper
+class Positive:
+    def __init__(self, name):
+        self.name = name
+
+    def __get__(self, obj, owner):
+        print("Сработал get")
+        return obj.__dict__[self.name]
+
+    def __set__(self, obj, value):
+        print("Сработал set")
+        if value <= 0:
+            raise ValueError(f"{self.name} должено быть  > 0")
+        obj.__dict__[self.name] = value
 
 
-class Service:
-    """Сервисный"""
-
-    @log_call
-    def process(self, x: float) -> float:
-        return x * 2
+class Product:
+    price = Positive("price")
 
 
-s = Service()
-s.process(10)
-
-
-def a():
-    return 1
-
-
-print(a.__name__)
-print(s.process.__name__)
+p = Product()
+p.price = 100
+print(p.price)
+p.price = -1
