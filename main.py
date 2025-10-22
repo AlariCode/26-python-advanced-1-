54bd5a33c3191f728a63f8a96d32ae02b103dba5
Alaricode <antonlarichev@gmail.com>
2025-10-22 17:03:13 +0300

    Упражнение — Декоратор limit

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 """Демо модуль для курса"""
 
-
-class Commands:
-    def start(self): print("Старт")
-    def stop(self): print("Стоп")
-    def help(self): print("Помощь")
+from functools import wraps
 
 
-cmd = Commands()
-action = input("Введите команду: ")
+def limit_calls(max_calls: int):
+    def decorator(fn):
+        @wraps(fn)
+        def wrapper(self, *args, **kwargs):
+            count_arrt = f"_{fn.__name__}_count"
+            current = getattr(self, count_arrt, 0)
+            if current >= max_calls:
+                raise RuntimeError("Call limit exceeded")
+            setattr(self, count_arrt, current + 1)
+            print(f"[LOG] {fn.__qualname__} called {current + 1}/{max_calls}")
+            return fn(self, *args, **kwargs)
+        return wrapper
+    return decorator
 
-if hasattr(cmd, action):
-    getattr(cmd, action)()
-else:
-    print("Команда не найдена")
+
+class Engine:
+    """Двигатель"""
+
+    @limit_calls(3)
+    def start(self):
+        """Запуск"""
+        print("🚗 Двигатель запущен!")
+
+
+car = Engine()
+
+car.start()
+car.start()
+car.start()
+car.start()  # <-- Ошибка Runtime Error
