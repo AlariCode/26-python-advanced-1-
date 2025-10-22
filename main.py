c11af3c872fb7332a9ebf35e9900b4dc94fdae9c
Alaricode <antonlarichev@gmail.com>
2025-10-22 16:52:34 +0300

    getattr и setattr

--- main.py
+++ main.py
@@ -1,26 +1,16 @@
 """Демо модуль для курса"""
 
 
-class Positive:
-    def __init__(self, name):
-        self.name = name
-
-    def __get__(self, obj, owner):
-        print("Сработал get")
-        return obj.__dict__[self.name]
-
-    def __set__(self, obj, value):
-        print("Сработал set")
-        if value <= 0:
-            raise ValueError(f"{self.name} должено быть  > 0")
-        obj.__dict__[self.name] = value
+class Commands:
+    def start(self): print("Старт")
+    def stop(self): print("Стоп")
+    def help(self): print("Помощь")
 
 
-class Product:
-    price = Positive("price")
+cmd = Commands()
+action = input("Введите команду: ")
 
-
-p = Product()
-p.price = 100
-print(p.price)
-p.price = -1
+if hasattr(cmd, action):
+    getattr(cmd, action)()
+else:
+    print("Команда не найдена")
