f8944801f34daab513a43be65d5623b6717f286a
Alaricode <antonlarichev@gmail.com>
2025-10-28 11:22:27 +0300

    Полиморфизм

--- main.py
+++ main.py
@@ -1,51 +1,30 @@
 """Демо модуль для курса"""
 
-# Делаем класс Курса с ценой, названием, длительностью. Методы:
-# - узнать цену
-# - вывести информацию
-# Делаем курс с AI и тренажёрам
-# - можно расчитать рассрочку на срок курса
-# Делаем курс с проектом с параметром названия проекта
-# - можно расчитать рассрочку на срок курса
-# - можно вывести информацию о проекте
+len("Purple")
+len([1, 2, 3])
+len({1: "a", 2: "b"})
 
 
-class CreditMixin:
-    """Миксин кредита"""
-    price = 0
-    length = 0
-
-    def calculate_credit(self):
-        return self.price / self.length
+class Payment:
+    def pay(self, amount):
+        raise NotImplementedError("Метод должен быть определён")
 
 
-class Course:
-    def __init__(self, name: str, price: float, lenght: int):
-        self.name = name
-        self.price = price
-        self.length = lenght
-
-    def get_price(self):
-        return self.price
-
-    def get_info(self):
-        return f"курс {self.name} по цене {self.price} длительностью {self.length}"
+class CardPayment(Payment):
+    def pay(self, amount):
+        print(f"Оплата картой: {amount}")
 
 
-class AICourse(Course, CreditMixin):
-    pass
+class CryptoPayment(Payment):
+    def pay(self, amount):
+        print(f"Оплата криптой: {amount}")
 
 
-class ProjectCourse(Course, CreditMixin):
-    def __init__(self, name: str, price: float, lenght: int, project_name: str):
-        super().__init__(name, price, lenght)
-        self.project_name = project_name
-
-    def get_project_info(self):
-        return f"Проект: {self.project_name}"
+class ApplePayment(Payment):
+    def pay(self, amount):
+        print(f"Оплата apple: {amount}")
 
 
-course = ProjectCourse("Python", 10000, 2, "Калькулятор")
-print(course.get_info())
-print(course.get_project_info())
-print(course.calculate_credit())
+payments = [CardPayment(), CryptoPayment(), ApplePayment()]
+for p in payments:
+    p.pay(100)
