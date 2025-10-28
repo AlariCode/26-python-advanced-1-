3533f53bcc51ff20df08c781c55a0adc494864e0
Alaricode <antonlarichev@gmail.com>
2025-10-28 14:16:27 +0300

    Упражнение - Расчёт скидки

--- main.py
+++ main.py
@@ -1,24 +1,63 @@
 """Демо модуль для курса"""
 
-# Notification - ...
-# EmailNotification - отправка через email
+# Заказ в интернет магазине
+# Item - name, price, qty и метод subtotal() -> считает цену
+# Политики скидок - NoDiscount - без скидки, PercentageDiscount - процент от заказа
+# Order - list[Item] и политика скидок, метод расчёта total total_with_discount и set_policy
+
+from dataclasses import dataclass
 
 
-class Notification:
-    def __init__(self, sender):
-        self.sender = sender
+@dataclass
+class Item:
+    """Единица товара"""
+    name: str
+    price: float
+    qty: int = 1
 
-    def send(self, message):
-        self.sender.send(message)
-
-    def get_ack(self):
-        pass
-
-
-class EmailSender:
-    def send(self, message):
-        print(f"Отправлно сообщение {message}")
+    def subtotal(self) -> float:
+        """Расчёт суммы"""
+        return self.price * self.qty
 
 
-notification = Notification(EmailSender())
-notification.send("Сообщение")
+class NoDiscount:
+    """Политика без скидки"""
+
+    def discount(self, total: float) -> float:
+        return 0
+
+
+@dataclass
+class PercentageDiscount:
+    """Политика с % скидки"""
+    percent: float
+
+    def discount(self, total: float) -> float:
+        return total * (self.percent / 100)
+
+
+class Order:
+    """Заказ"""
+
+    def __init__(self, items: list[Item], policy) -> None:
+        self.items = items
+        self.policy = policy
+
+    def total(self):
+        return sum(i.subtotal() for i in self.items)
+
+    def total_with_discount(self):
+        t = self.total()
+        return t - self.policy.discount(t)
+
+    def set_policy(self, policy):
+        self.policy = policy
+
+
+basket = [Item("Бумага", 100, 5), Item("Ершик", 1000, 1),]
+order = Order(basket, NoDiscount())
+print(order.total())
+print(order.total_with_discount())
+order.set_policy(PercentageDiscount(10))
+print(order.total())
+print(order.total_with_discount())
