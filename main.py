dae2dc66b84c4ab21f5e14b399d79c0d9fa6b615
Alaricode <antonlarichev@gmail.com>
2025-10-30 16:59:13 +0300

    Абстрактный класс

--- main.py
+++ main.py
@@ -5,32 +5,40 @@
 # Приложение чистает строку и передаёт в use_storage, сохарняет в одном из storage
 # После успешного сохранения читает storage и выводит сохранённые данные
 
-from typing import Protocol
+from abc import ABC, abstractmethod
 
 
-class Storage(Protocol):
+class Storage(ABC):
     """Протокол хранения"""
 
+    @abstractmethod
     def save(self, data: str) -> None: ...
+
+    @abstractmethod
     def load(self) -> str: ...
 
+    def log(self):
+        print("Сохранено")
 
-class MemoryStorage:
+
+class MemoryStorage(Storage):
     """Хранение в памяти"""
 
     def save(self, data: str) -> None:
         self.data = data
+        self.log()
 
     def load(self) -> str:
         return getattr(self, "data", "")
 
 
-class FileStorage:
+class FileStorage(Storage):
     """Хранение в файле"""
 
     def save(self, data: str) -> None:
         with open("data.txt", "w", encoding="utf-8") as f:
             f.write(data)
+            self.log()
 
     def load(self) -> str:
         with open("data.txt", "r", encoding="utf-8") as f:
