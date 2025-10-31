d97c809193bfb5aecc7cbf28d514a2f30cbb6378
Alaricode <antonlarichev@gmail.com>
2025-10-31 09:47:38 +0300

    Single Responsibility Principle

--- main.py
+++ main.py
@@ -1,58 +1,24 @@
 """Демо модуль для курса"""
 
-# Хранилище
-# Нужно реалозовать MemoryStorage и FileStorage с методами load и save
-# Приложение чистает строку и передаёт в use_storage, сохарняет в одном из storage
-# После успешного сохранения читает storage и выводит сохранённые данные
+# Single Responsibility Principle (SRP)
+# Класс должен иметь только одну причину для изменения.
 
-from abc import ABC, abstractmethod
+from dataclasses import dataclass
 
 
-class Storage(ABC):
-    """Протокол хранения"""
+@dataclass
+class Order:
+    items: list[str]
 
-    @abstractmethod
-    def save(self, data: str) -> None: ...
-
-    @abstractmethod
-    def load(self) -> str: ...
-
-    def log(self):
-        print("Сохранено")
+    def calculate_total(self):
+        return len(self.items) * 10
 
 
-class MemoryStorage(Storage):
-    """Хранение в памяти"""
-
-    def save(self, data: str) -> None:
-        self.data = data
-        self.log()
-
-    def load(self) -> str:
-        return getattr(self, "data", "")
+class OrderRepository:
+    def save_to_db(self, order: Order):
+        print("Сохранение в базу")
 
 
-class FileStorage(Storage):
-    """Хранение в файле"""
-
-    def save(self, data: str) -> None:
-        with open("data.txt", "w", encoding="utf-8") as f:
-            f.write(data)
-            self.log()
-
-    def load(self) -> str:
-        with open("data.txt", "r", encoding="utf-8") as f:
-            return f.read()
-
-
-def use_storage(storage: Storage, data: str):
-    storage.save(data)
-    return storage.load()
-
-
-mem = MemoryStorage()
-file = FileStorage()
-
-user_input = input("Введите данные: ")
-print(use_storage(mem, user_input))
-print(use_storage(file, user_input))
+class EmailService:
+    def send_confirmation_email(self, email: str, order: Order):
+        print("Отправка письма")
