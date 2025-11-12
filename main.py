7eb3c0ed77271ae2995524ff31374523650da589
Alaricode <antonlarichev@gmail.com>
2025-11-12 15:46:28 +0300

    Модель объектов

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-import dis
+from typing import Any
 
 
-def pr(a):
-    return a + 1
+class Demo:
+    def __getattribute__(self, name: str) -> Any:
+        print(f"Доступ к {name}")
+        return super().__getattribute__(name)
+
+    def __getattr__(self, name):
+        print(f"{name} на найден")
+        return None
+
+    def hello(self):
+        print("привет!", self)
 
 
-def sum_new(a, b):
-    c = a + b
-    d = pr(c)
-    return d
+d = Demo()
+d.x = 42
+print(d.x)
+print(d.y)
 
-
-x = sum_new(1, 2)
-
-dis.dis(sum_new)
+print(d.__dict__)
+print(d.hello)
+print(Demo.hello)
