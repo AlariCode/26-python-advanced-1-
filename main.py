16a466b7a1162b8e965e668e29914cbd07751cc8
Alaricode <antonlarichev@gmail.com>
2025-11-12 19:51:19 +0300

    Slots

--- main.py
+++ main.py
@@ -1,24 +1,50 @@
-from typing import Any
+from dataclasses import dataclass
+import sys
 
 
-class Demo:
-    def __getattribute__(self, name: str) -> Any:
-        print(f"Доступ к {name}")
-        return super().__getattribute__(name)
-
-    def __getattr__(self, name):
-        print(f"{name} на найден")
-        return None
-
-    def hello(self):
-        print("привет!", self)
+class A:
+    def __init__(self) -> None:
+        self.x = 1
+        self.y = 1
 
 
-d = Demo()
-d.x = 42
-print(d.x)
-print(d.y)
+a = A()
 
-print(d.__dict__)
-print(d.hello)
-print(Demo.hello)
+
+class B:
+    __slots__ = ("x", "y")
+
+    def __init__(self) -> None:
+        self.x = 1
+        self.y = 1
+
+
+b = B()
+print(b.x)
+print(b.y)
+# b.z = 1
+
+print(sys.getsizeof(a))
+print(sys.getsizeof(a.__dict__))
+print(sys.getsizeof(b))
+
+
+@dataclass(slots=True)
+class Point:
+    x: int
+    y: int
+
+
+p = Point(1, 1)
+
+
+class D3Point(Point):
+    __slots__ = ("z",)
+
+
+class MyPoint(Point):
+    pass
+
+
+p2 = MyPoint(1, 2)
+print(p2.__dict__)
