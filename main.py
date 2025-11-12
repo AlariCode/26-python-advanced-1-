e2ec626231b0cfaee1ae92663456f35bc3608b6e
Alaricode <antonlarichev@gmail.com>
2025-11-12 20:16:13 +0300

    Память и сборка мусора

--- main.py
+++ main.py
@@ -1,50 +1,10 @@
-from dataclasses import dataclass
-import sys
+
+import gc
 
 
-class A:
-    def __init__(self) -> None:
-        self.x = 1
-        self.y = 1
+a = []
+b = [a]
+a.append(b)
 
-
-a = A()
-
-
-class B:
-    __slots__ = ("x", "y")
-
-    def __init__(self) -> None:
-        self.x = 1
-        self.y = 1
-
-
-b = B()
-print(b.x)
-print(b.y)
-# b.z = 1
-
-print(sys.getsizeof(a))
-print(sys.getsizeof(a.__dict__))
-print(sys.getsizeof(b))
-
-
-@dataclass(slots=True)
-class Point:
-    x: int
-    y: int
-
-
-p = Point(1, 1)
-
-
-class D3Point(Point):
-    __slots__ = ("z",)
-
-
-class MyPoint(Point):
-    pass
-
-
-p2 = MyPoint(1, 2)
-print(p2.__dict__)
+print(gc.get_stats())
+gc.collect()
