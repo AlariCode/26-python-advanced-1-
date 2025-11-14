1e085124df858ff11c575d2d4f846b659d49b51c
Alaricode <antonlarichev@gmail.com>
2025-11-14 12:26:44 +0300

    Упражнение - Работа с tuple

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
+# make_pair - создаёт Tuple из двух значений
+# get_first - достаёт первый элемент из пары
+# get_second - достаёт второй элемент из пары
+# swap_pair - меняет элементы местами
 
-from typing import TypeVar, Callable
+from typing import TypeVar
 
 
 T = TypeVar("T")
 R = TypeVar("R")
 
 
-def process_items(items: list[T], transformer: Callable[[T], R]) -> list[R]:
-    return [transformer(item) for item in items]
+def make_pair(a: T, b: R) -> tuple[T, R]:
+    return (a, b)
 
 
-def to_upper(s: str) -> str:
-    return s.upper()
+def get_first(pair: tuple[T, R]) -> T:
+    return pair[0]
 
 
-result = process_items(["Anton", "Vasia"], to_upper)
+def get_second(pair: tuple[T, R]) -> R:
+    return pair[1]
+
+
+def swap_pair(pair: tuple[T, R]) -> tuple[R, T]:
+    return (pair[1], pair[0])
