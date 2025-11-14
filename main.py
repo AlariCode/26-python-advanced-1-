61248bc31390e0cba093a19e7b34c1747800e492
Alaricode <antonlarichev@gmail.com>
2025-11-14 15:03:21 +0300

    Упражнение - Репозиторий

--- main.py
+++ main.py
@@ -1,34 +1,31 @@
+# Нужно сделать Repository, который работает с любымим типами и имеет методы:
+# add - добавляет в список элемент
+# get_by_index - получает по index
+# get_all - получает все
+
+# Всё хранится как  list
 
 from dataclasses import dataclass
-from typing import Generic, TypeVar
-
-
-@dataclass
-class IntBox:
-    value: int
-
-    def get(self) -> int:
-        return self.value
-
-
-@dataclass
-class StrBox:
-    value: str
-
-    def get(self) -> str:
-        return self.value
-
+from typing import Generic, TypeVar, Optional
 
 T = TypeVar("T")
 
 
 @dataclass
-class Box(Generic[T]):
-    value: T
+class Repository(Generic[T]):
+    items: list[T]
 
-    def get(self) -> T:
-        return self.value
+    def add(self, item: T):
+        self.items.append(item)
+
+    def det_by_index(self, index: int) -> Optional[T]:
+        if 0 <= index < len(self.items):
+            return self.items[index]
+        return None
+
+    def get_all(self) -> list[T]:
+        return self.items
 
 
-int_box = Box(1)
-int_box.get()
+repo = Repository[str](["a", "b"])
+print(repo.det_by_index(2))
