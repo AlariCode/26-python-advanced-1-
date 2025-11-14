a42eff8c2b477affbb257c23b09288d787530fb8
Alaricode <antonlarichev@gmail.com>
2025-11-14 14:48:21 +0300

    Generic Class

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
-# Сделать функции
-# safe_get - безопасное получение элемента по индексу
-# map_optional - Применяет функцию к значению, если оно не None
-# or_else - Возвращает значение или default если None
 
-from typing import TypeVar, Optional, Callable
+from dataclasses import dataclass
+from typing import Generic, TypeVar
+
+
+@dataclass
+class IntBox:
+    value: int
+
+    def get(self) -> int:
+        return self.value
+
+
+@dataclass
+class StrBox:
+    value: str
+
+    def get(self) -> str:
+        return self.value
 
 
 T = TypeVar("T")
-R = TypeVar("R")
 
 
-def safe_get(items: list[T], index: int) -> Optional[T]:
-    if 0 <= index <= len(items):
-        return items[index]
-    return None
+@dataclass
+class Box(Generic[T]):
+    value: T
+
+    def get(self) -> T:
+        return self.value
 
 
-def map_optional(value: Optional[T], transformer: Callable[[T], R]) -> Optional[R]:
-    if value is not None:
-        return transformer(value)
-    return None
-
-
-def or_else(value: Optional[T], default: T) -> T:
-    if value is not None:
-        return value
-    return default
+int_box = Box(1)
+int_box.get()
