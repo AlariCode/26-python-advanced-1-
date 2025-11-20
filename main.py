e080c7a3f3219a42ce68f59ba51d5a738520a8f9
Alaricode <antonlarichev@gmail.com>
2025-11-20 11:18:04 +0300

    Упражнение - Поиска по id

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
-
+# Есть User, Product и Order у которых есть поле id.
+# Нужно сделать универвальную функицю поиска по id
 
 from dataclasses import dataclass
-from typing import Generic, Protocol, TypeVar
+from typing import Optional, Protocol, TypeVar
 
 
-class Runnable(Protocol):
-    def run(self) -> None: ...
+class Identifieble(Protocol):
+    id: int
 
 
-T = TypeVar("T", bound=Runnable)
+T = TypeVar("T", bound=Identifieble)
 
 
 @dataclass
-class TaskRunner(Generic[T]):
-    tasks: list[T]
+class User:
+    id: int
+    email: str
 
-    def run_all(self) -> None:
-        for task in self.tasks:
-            task.run()
+
+@dataclass
+class Product:
+    id: int
+    title: str
+
+
+@dataclass
+class Order:
+    id: int
+    producs: list[Product]
+
+
+def get_by_id(items: list[T], id_: int) -> Optional[T]:
+    for item in items:
+        if item.id == id_:
+            return item
+    return None
+
+
+get_by_id([User(1, "a@a.ru")], 1)
