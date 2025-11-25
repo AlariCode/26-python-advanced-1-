f5885ac173e949b7c28661daba6b1348bb4e44e5
Alaricode <antonlarichev@gmail.com>
2025-11-25 17:10:04 +0300

    wait и wait_for

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ import aiohttp
 # Получение в задачам параллельно 10 раз обращение к google
 
 
+async def fetch(session, url):
+    return await asyncio.wait_for(session.get(url), timeout=2)
+
+
 async def main():
-    urls = ["https://google.com"] * 20
+    urls = ["https://google.com"] * 10
     async with aiohttp.ClientSession() as session:
         tasks = [
-            session.get(url) for url in urls
+            asyncio.create_task(fetch(session, url)) for url in urls
         ]
-        results = await asyncio.gather(*tasks)
+        done, _ = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
+        results = [task.result() for task in done]
         print(list(map(lambda x: x.status, results)))
 
 asyncio.run(main())
