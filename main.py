2b98e89169bd9c92f23b05739ba3edbf989a299c
Alaricode <antonlarichev@gmail.com>
2025-11-26 10:59:23 +0300

    shield

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import asyncio
 
 
+async def save():
+    print("Сохраняю")
+    await asyncio.sleep(2)
+    print("Сохранено")
+
+
 async def job():
     print("Работаю")
+    t = asyncio.create_task(save())
+    await asyncio.shield(t)
+    # await t
     await asyncio.sleep(5)
     print("Готово!")
 
@@ -16,5 +25,7 @@ async def main():
     except asyncio.CancelledError:
         print(task.cancelled())
         print("Задача отменена")
+    await asyncio.sleep(4)
+
 
 asyncio.run(main())
