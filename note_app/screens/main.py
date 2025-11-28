e404ad20dda6e8efd5091bc17a98c7995b209d58
Alaricode <antonlarichev@gmail.com>
2025-11-28 15:35:03 +0300

    Bindings

--- note_app/screens/main.py
+++ note_app/screens/main.py
@@ -1,8 +1,19 @@
 from textual.app import ComposeResult
 from textual.screen import Screen
-from textual.widgets import Header
+from textual.widgets import Header, Footer
 
 
 class MainScreen(Screen):
+    BINDINGS = [
+        ("q", "quit", "Выход")
+    ]
+
     def compose(self) -> ComposeResult:
         yield Header()
+        yield Footer()
+
+    def on_mount(self):
+        self.title = "Менеджер заметок"
+
+    def action_quit(self):
+        self.app.exit()
