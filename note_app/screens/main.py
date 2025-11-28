ef4c2a15cf565765df47d85350efc6b16e4bc09e
Alaricode <antonlarichev@gmail.com>
2025-11-28 16:06:35 +0300

    Контейнеры и стили

--- note_app/screens/main.py
+++ note_app/screens/main.py
@@ -1,15 +1,25 @@
 from textual.app import ComposeResult
 from textual.screen import Screen
-from textual.widgets import Header, Footer
+from textual.widgets import Header, Footer, Tree, Markdown
+from textual.containers import Horizontal
 
 
 class MainScreen(Screen):
+    CSS = """
+    #tree {
+        width: 25%
+    }
+    """
+
     BINDINGS = [
         ("q", "quit", "Выход")
     ]
 
     def compose(self) -> ComposeResult:
         yield Header()
+        with Horizontal():
+            yield Tree(label="Моя база знаний", id="tree")
+            yield Markdown("# Привет! Я загловок")
         yield Footer()
 
     def on_mount(self):
